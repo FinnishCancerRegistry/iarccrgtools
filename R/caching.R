38a9798f41567845776dd5a39a043d4b84a24a5a
Joonas Miettinen <joonas.miettinen@cancer.fi>
2022-04-08 16:54:12 +0300

    exported and documented cache_* funs.

--- R/caching.R
+++ R/caching.R
@@ -1,15 +1,40 @@
 
+#' @title Cache Metadata
+#' @description
+#' Tools to access and handle cache metadata. These are advanced /
+#' under-the-hood tools which you almost certainly should not use for anything.
+#' `[iarccrgtools::cache_clean_hash]` and
+#' `[iarccrgtools::cache_clean_all]` may be the exceptions.
+#' @name cache_metadata
+NULL
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_metadata_file_path]` returns a string, the name of
+#' to the cache metadata file.
 cache_metadata_file_name <- function() {
   "iarccrgtools_cache_info.csv"
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_metadata_file_path]` returns a string, the path
+#' to the cache metadata.
 cache_metadata_file_path <- function() {
   dir_path <- iarccrgtools::get_tools_work_dir()
   file_path <- paste0(dir_path, "/", cache_metadata_file_name())
   normalize_path(file_path)
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_metadata_read]` reads cache metadata from the path
+#' given by
+#' `[iarccrgtools::cache_metadata_file_path]`. Returns a `data.table` with
+#' zero rows if no file found.
 cache_metadata_read <- function() {
   file_path <- cache_metadata_file_path()
   if (file.exists(file_path)) {
@@ -24,10 +49,36 @@ cache_metadata_read <- function() {
   }
 }
 
-cache_metadata_write <- function(x) {
-  data.table::fwrite(x, file = cache_metadata_file_path())
+#' @rdname cache_metadata
+#' @export
+#' @param metadata `[data.frame]` (no default)
+#'
+#' Metadata.
+#' @section Functions:
+#' `[iarccrgtools::cache_metadata_write]` writes `metadata` to the path given by
+#' `[iarccrgtools::cache_metadata_file_path]`.
+cache_metadata_write <- function(metadata) {
+  stopifnot(
+    is.data.frame(metadata)
+  )
+  data.table::fwrite(metadata, file = cache_metadata_file_path())
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @param hash `[character]` (no default)
+#'
+#' Hash of an input dataset.
+#' @param working.dir `[character]` (no default)
+#'
+#' Path to directory to include in metadata.
+#' @param input.file.path `[character]` (no default)
+#'
+#' Path to input file (which must exist) to include in metadata.
+#' @section Functions:
+#' `[iarccrgtools::cache_metadata_append_or_replace]` appends data to
+#' the cache metadata, or replaces data that already exists for the
+#' same input file path, if any.
 cache_metadata_append_or_replace <- function(
   hash,
   working.dir,
@@ -68,30 +119,59 @@ cache_metadata_append_or_replace <- function(
   cache_metadata_write(cache_metadata)
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_metadata_refresh]` removes any non-existing
+#' directories from the cache metadata.
 cache_metadata_refresh <- function() {
   cache_metadata <- cache_metadata_read()
   cache_metadata <- cache_metadata[dir.exists(cache_metadata[["dir_path"]]), ]
   cache_metadata_write(cache_metadata)
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_hash_random]` outputs a random hash.
 cache_hash_random <- function() {
   cache_hash(runif(n = 1L))
 }
-cache_hash <- function(x) {
-  digest::digest(x, algo = "sha256")
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_hash]` outputs a hash for the input `data.frame`.
+#' @param df `[data.frame]` (no default)
+#' `data.frame` for which to compute hash.
+cache_hash <- function(df) {
+  stopifnot(
+    is.data.frame(df)
+  )
+  digest::digest(df, algo = "sha256")
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_clean_hash]` removes all cache dirs for the given hash.
+#' Also cleans up the cache metadata.
 cache_clean_hash <- function(hash) {
   meta <- cache_metadata_read()
   has_hash <- meta[["hash"]] == hash
-  dir_path <- meta[["dir_path"]][has_hash]
-  if (length(dir_path) == 1 && dir.exists(dir_path)) {
-    unlink(dir_path, recursive = TRUE, force = TRUE)
+  dir_paths <- meta[["dir_path"]][has_hash]
+  dir_paths <- dir_paths[dir.exists(dir_paths)]
+  if (length(dir_paths) > 0) {
+    unlink(dir_paths, recursive = TRUE, force = TRUE)
   }
   meta <- meta[!has_hash, ]
   cache_metadata_write(meta)
 }
 
+#' @rdname cache_metadata
+#' @export
+#' @section Functions:
+#' `[iarccrgtools::cache_clean_all]` removes all cache dirs.
+#' Also cleans up the cache metadata.
 cache_clean_all <- function() {
   meta <- cache_metadata_read()
   dir_paths <- meta[["dir_path"]]
