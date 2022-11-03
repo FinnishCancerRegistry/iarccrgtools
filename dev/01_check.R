f5442f3cea969b050751b92d3d89c562d95ac862
Joonas Miettinen <joonas.miettinen@cancer.fi>
2022-11-03 10:50:00 +0200

    build: add dev utils

---
+++ dev/01_check.R
@@ -0,0 +1,5 @@
+
+library("devtools")
+
+chk <- devtools::check()
+print(chk)
---
+++ dev/02_release.R
@@ -0,0 +1,15 @@
+
+library("usethis")
+
+# increment version + commit
+usethis::use_version()
+
+# tag this version with the command:
+# git tag vX.Y.Z
+# e.g.
+# git tag v0.2.28
+
+# remember to push commits with e.g.
+# git push
+# and to push tags with
+# git push --tags
