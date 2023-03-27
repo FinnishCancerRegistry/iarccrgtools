f0e90e9501bced41a0b7b88b295a53443d5e8511
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-03-27 21:30:41 +0300

    doc: add v0.2.30 news

--- R/docs_for_deleted_funs.R
+++ R/docs_for_deleted_funs.R
@@ -2,3 +2,22 @@
 # `iarccrgtools::create_example` permanently removed. Use
 # `iarccrgtools::tool_colnameset_example_dataset`.
 # @codedoc_comment_block news("iarccrgtools::create_example", "2023-03-27", "0.2.29")
+
+# @codedoc_comment_block news("iarccrgtools::write_fwf", "2023-03-27", "0.2.30")
+# `iarccrgtools::write_fwf` permanently removed. Use
+# `fwf::fwf_write`.
+# @codedoc_comment_block news("iarccrgtools::write_fwf", "2023-03-27", "0.2.30")
+
+# @codedoc_comment_block news("iarccrgtools::read_fwf", "2023-03-27", "0.2.30")
+# `iarccrgtools::read_fwf` permanently removed. Use
+# `fwf::fwf_read`.
+# @codedoc_comment_block news("iarccrgtools::read_fwf", "2023-03-27", "0.2.30")
+
+# @codedoc_comment_block news("iarccrgtools::read_fwf", "2023-03-27", "0.2.30")
+# `iarccrgtools::read_fwf` permanently removed. Use
+# `fwf::fwf_read`.
+# @codedoc_comment_block news("iarccrgtools::read_fwf", "2023-03-27", "0.2.30")
+
+# @codedoc_comment_block news("iarccrgtools::format_fwf", "2023-03-27", "0.2.30")
+# `iarccrgtools::format_fwf` permanently removed.
+# @codedoc_comment_block news("iarccrgtools::format_fwf", "2023-03-27", "0.2.30")
