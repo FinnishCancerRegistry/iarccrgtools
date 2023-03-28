e9b2dd1a0fa80abc2688c8fe6ffd3eaa27e7f86e
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-03-28 18:45:38 +0300

    build: improve dev/01

--- dev/01_check.R
+++ dev/01_check.R
@@ -3,3 +3,9 @@ library("devtools")
 
 chk <- devtools::check()
 print(chk)
+
+devtools::load_all()
+
+if (grepl("[.]exe$", iarccrgtools::iarc_exe_path_get(), ignore.case = TRUE)) {
+  source("tests/testthat/test_auto.R")
+}
